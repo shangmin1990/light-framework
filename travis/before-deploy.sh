1cc213aed0f8151606f83deb09d0611bf3eefbb1
shangmin <shangmin@le.com>
2017-07-31 11:19:25 +0800

    travis build trigger

--- travis/before-deploy.sh
+++ travis/before-deploy.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/env bash
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
-   openssl aes-256-cbc -K $encrypted_11b14bfd4b2e_key -iv $encrypted_11b14bfd4b2e_iv -in codesigning.asc.enc -out codesigning.asc -d 
-   gpg --fast-import codesigning.asc
+   openssl aes-256-cbc -K $encrypted_11b14bfd4b2e_key -iv $encrypted_11b14bfd4b2e_iv -in $TRAVIS_BUILD_DIR/travis/codesigning.asc.enc -out $TRAVIS_BUILD_DIR/travis/codesigning.asc -d
+   gpg --fast-import travis/codesigning.asc
 fi
