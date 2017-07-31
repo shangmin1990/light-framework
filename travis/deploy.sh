907006f2f00aed129532a6ffa5723ca805493a8f
shangmin <shangmin@le.com>
2017-07-31 13:47:21 +0800

    release TAG auto build

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -2,3 +2,7 @@
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
     mvn clean deploy --settings travis/mvn_settings.xml
 fi
+
+if [[ "$TRAVIS_TAG" =~ "*-RELEASE" ]]; then
+    mvn clean deploy --settings travis/mvn_settings.xml -P release
+fi
