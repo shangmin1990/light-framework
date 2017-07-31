694c24ec8d55759c4fc3da566470384326842d00
shangmin <shangmin@le.com>
2017-07-31 14:07:13 +0800

    release时 travis build

--- travis/deploy.sh
+++ travis/deploy.sh
@@ -1,8 +1,15 @@
-#!/usr/bin/env bash
+#!/bin/bash
+TRAVIS_BRANCH="abc"
+TRAVIS_TAG="2.1.0-RELEASE"
+RELEASE="-RELEASE"
 if [ "$TRAVIS_BRANCH" = 'master' ] && [ "$TRAVIS_PULL_REQUEST" == 'false' ]; then
+    echo "master build"
     mvn clean deploy --settings travis/mvn_settings.xml
 fi
 
-if [[ "$TRAVIS_TAG" =~ "*-RELEASE" ]]; then
+if [[ "$TRAVIS_TAG" == *$RELEASE ]]; then
+    echo "release build"
     mvn clean deploy --settings travis/mvn_settings.xml -P release
+else 
+    echo "不包含"
 fi
